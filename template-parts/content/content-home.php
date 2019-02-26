ebadaf9b698af7a76e339e961ad865f26ef150a7
Anna R Pack <annarpack@gmail.com>
2019-02-25 21:51:30 -0500

    new theme

---
+++ template-parts/content/content-home.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+
+
+<article class="section-traits"  >
+
+		<div class="home-content">
+			<h3>Traits</h3>
+			<div class="traits-pie">
+				<div class="labels-left">
+					<label class="pie-labels">Great Communicator</label>
+					<div class="pie-labels-line"></div>
+					<label class="pie-labels">Creative</label>
+					<div class="pie-labels-line"></div>
+				</div>
+			<div class="shape-circle">
+				<div class="pie-chart">
+					<div class="pie" data-start="0" data-value="10" ></div>
+					<div class="pie" data-start="10" data-value="12"></div>
+					<div class="pie" data-start="22" data-value="14"></div>
+					<div class="pie" data-start="36" data-value="40"></div>
+					<div class="pie" data-start="76" data-value="24"></div>
+				</div> <!-- //pie-chart  -->
+			</div> <!-- // shape-circle -->
+			<div class="labels-right">
+				<label class="pie-labels">Self Driven</label>
+				<div class="pie-labels-line"></div>
+				<label class="pie-labels">Organized</label>
+				<div class="pie-labels-line"></div>
+				<label class="pie-labels">Problem Solving</label>
+				<div class="pie-labels-line"></div>
+			</div>
+		</div> <!-- //traits-pie -->
+		</div>
+
+</article>
+
+  <article class="section-icons">
+  <div class="section-skills-list home-content">
+					<h3>Skills</h3>
+    <ul class="section-skills-imgs">
+      <li>
+        <h6 class="hidden-titles">HTML5</h6>
+        <img src="http://www.annarpack.com/wp-content/uploads/2017/11/html.png" class="icons" /></li>
+      <li>
+        <h6  class="hidden-titles">CSS3</h6>
+        <img src="http://www.annarpack.com/wp-content/uploads/2017/11/css.png" class="icons" /></li>
+      <li>
+        <h6  class="hidden-titles">JavaScript ES6</h6>
+        <img src="http://www.annarpack.com/wp-content/uploads/2017/11/js.png" class="icons" /></li>
+      <li>
+        <h6 class="hidden-titles">jQuery</h6>
+        <img src="http://www.annarpack.com/wp-content/uploads/2017/11/jquery-icon.png" class="icons" /></li>
+      <li>
+        <h6  class="hidden-titles">Node.js</h6>
+        <img src="http://www.annarpack.com/wp-content/uploads/2017/11/node_js.png" class="icons" /></li>
+      <li>
+        <h6  class="hidden-titles">React.js</h6>
+        <img src="http://www.annarpack.com/wp-content/uploads/2017/11/react_js.png" class="icons" /></li>
+      <li>
+        <h6  class="hidden-titles">Git</h6>
+        <img src="http://www.annarpack.com/wp-content/uploads/2017/11/git.png" class="icons" /></li>
+      <li>
+        <h6  class="hidden-titles">Ruby on Rails</h6>
+        <img src="http://www.annarpack.com/wp-content/uploads/2017/11/rails.png" class="icons" /></li>
+      <li>
+        <h6  class="hidden-titles">Wordpress 5</h6>
+        <img src="http://www.annarpack.com/wp-content/uploads/2017/11/wordpress.png" class="icons" /></li>
+      <li>
+        <h6  class="hidden-titles">PHP 7</h6>
+        <img src="http://www.annarpack.com/wp-content/uploads/2017/11/php.png" class="icons" /></li>
+    </ul>
+  </div>
+  </article>
+
+<article class="section-experience" >
+		<div class="home-content">
+				<h3>Experience</h3>
+			<div class="bar-graph">
+				<ul>
+					<div class="bar-line-item">
+						<li class="graph-lines">
+							<div class="years years-5">5 YEARS </div>
+						</li>
+						<span class="labels">JavaScript</span>
+					</div>
+					<div class="bar-line-item">
+					<li class="graph-lines">
+						<div class="years years-3">3 YEARS </div>
+					 </li>
+					 <span class="labels">Node.js</span>
+				</div>
+				<div class="bar-line-item">
+					<li class="graph-lines">
+						<div class="years years-3"> 3 YEARS </div>
+					 </li>
+					 <span class="labels">React.js</span>
+				</div>
+				<div class="bar-line-item">
+					<li class="graph-lines">
+						<div class="years years-5"> 5 YEARS </div>
+					 </li>
+					 	<span class="labels">jQuery</span>
+				</div>
+				<div class="bar-line-item">
+					<li class="graph-lines">
+						<div class="years years-3"> 3 YEARS </div>
+					 </li>
+					 	<span class="labels">Git</span>
+				</div>
+				<div class="bar-line-item">
+					<li class="graph-lines">
+						<div class="years years-7"> 7 YEARS </div>
+					 </li>
+					 <span class="labels">CSS</span>
+				</div>
+				<div class="bar-line-item">
+					<li class="graph-lines">
+						<div class="years years-10"> 10 YEARS </div>
+					 </li>
+					 <span class="labels">HTML</span>
+				</div>
+				<div class="bar-line-item">
+					<li class="graph-lines">
+						<div class="years years-7">  7 YEARS </div>
+					 </li>
+					 <span class="labels">PHP</span>
+				</div>
+				<div class="bar-line-item">
+					<li class="graph-lines">
+						<div class="years years-7">  7 YEARS </div>
+					 </li>
+					 <span class="labels">WordPress</span>
+				</div>
+				<div class="bar-line-item">
+					<li class="graph-lines">
+						<div class="years years-1"> 1 YEAR </div></li>
+							<span class="labels">Ruby</span>
+				</div>
+				</ul>
+
+
+
+			</div>
+		</div>
+</article>
